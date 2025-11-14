3fd807ab5f6e410ec0e2e94373ac1476c56d4a0e
lui15 <lpichardosanchez15@gmail.com>
2025-11-14 15:23:37 +0100

    feat: Mensaje de bienvenida actualizado

--- api/v1/halo.js
+++ api/v1/halo.js
@@ -1,6 +1,7 @@
 // api/v1/hola.js
 module.exports = (req, res) => {
   res.status(200).json({
-    mensaje: "¡Bienvenido a la versión 1 de nuestra API!",
+    // MENSAJE ACTUALIZADO
+    mensaje: "¡API actualizada y mejorada!",
   });
 };
